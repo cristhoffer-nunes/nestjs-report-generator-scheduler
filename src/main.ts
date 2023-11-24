242290a7b69b5276ad935f4d71223d1deb7fd8f2
cristhoffer-santos <cristhoffer.santos@jbq.global>
2023-11-24 15:52:20 -0300

    feat: add logs fron env variables

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import envVariablesConfig from './config/env-variables.config';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
+  const port = envVariablesConfig.PORT;
   const logger = new Logger(bootstrap.name);
+  logger.log(`Env Variables: ${JSON.stringify(envVariablesConfig)}`);
   await app.listen(port, () => {
     logger.log(`Service listening on port ${port}`);
   });
